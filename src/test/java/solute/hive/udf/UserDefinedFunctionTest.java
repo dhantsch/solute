b91a2a5a9fa4b72067690b3b3c970a1c8826af97
dhantsch <dhantsch@kybeidos.de>
2017-01-25 12:10:21 +0100

    add hive udf test

---
+++ src/test/java/solute/hive/udf/UserDefinedFunctionTest.java
@@ -0,0 +1,55 @@
+package solute.hive.udf;
+
+import com.klarna.hiverunner.StandaloneHiveRunner;
+import com.klarna.hiverunner.HiveShell;
+import com.klarna.hiverunner.annotations.HiveSQL;
+import com.klarna.hiverunner.annotations.HiveSetupScript;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(StandaloneHiveRunner.class)
+public class UserDefinedFunctionTest {
+
+
+    private final String hdfsSource = "${hiveconf:hadoop.tmp.dir}/udf";
+
+    @HiveSetupScript
+    String setup =
+            "  CREATE TABLE udf_test (" +
+                    " id int," +
+                    " value string" +
+                    "  )" +
+                    "  ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'" +
+                    "  STORED AS TEXTFILE" +
+                    "  LOCATION '" + hdfsSource + "' ";
+
+    @HiveSQL(files = {}, autoStart = false)
+    public HiveShell hiveShell;
+
+    @Test
+    public void udfMax() {
+        hiveShell.addResource(hdfsSource + "/data.csv","123\tv1\n124\tv2\n125\tv3");
+        hiveShell.start();
+        Assert.assertEquals(Arrays.asList("125"), hiveShell.executeQuery("SELECT max(id) FROM udf_test"));
+    }
+
+    @Test
+    public void udfMin() {
+        hiveShell.addResource(hdfsSource + "/data.csv","123\tv1\n124\tv2\n125\tv3");
+        hiveShell.start();
+        Assert.assertEquals(Arrays.asList("123"), hiveShell.executeQuery("SELECT min(id) FROM udf_test"));
+    }
+
+    @Test
+    public void regexp_extract() {
+        hiveShell.addResource(hdfsSource + "/data.csv","1\t123ABC");
+        hiveShell.start();
+        List<String> expected = Arrays.asList("123");
+        List<String> actual = hiveShell.executeQuery("SELECT regexp_extract(value, '([0-9]*)[A-Z]*', 1) FROM udf_test");
+        Assert.assertEquals(expected, actual);
+    }
+}
